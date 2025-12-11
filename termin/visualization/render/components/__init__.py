cf30997526d48560b4fdc1609c0bc927dce4ae99
koven <kovenwalker@proton.me>
2025-12-11 17:20:43 -0500

    a

--- termin/visualization/render/components/__init__.py
+++ termin/visualization/render/components/__init__.py
@@ -1,10 +1,11 @@
-
 from .mesh_renderer import MeshRenderer
 from .skybox_renderer import SkyboxRenderer
 from .line_renderer import LineRenderer
+from .light_component import LightComponent
 
 __all__ = [
     "MeshRenderer",
     "SkyboxRenderer",
     "LineRenderer",
+    "LightComponent",
 ]
