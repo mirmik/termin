f00a4d4508c9a168f6828260bf3535e2ced847bc
koven <kovenwalker@proton.me>
2025-12-12 05:44:59 -0500

    'a'

--- setup.py
+++ setup.py
@@ -11,51 +11,52 @@ import os
 directory = os.path.dirname(os.path.realpath(__file__))
 
 
-setup(
-    name="termin",
-    packages=["termin"],
-    python_requires='>3.10.0',
-    version="0.0.0",
-    license="MIT",
-    description="Projective geometry library",
-    author="mirmik",
-    author_email="mirmikns@yandex.ru",
-    url="https://github.com/mirmik/termin",
-    long_description=open(os.path.join(
-        directory, "README.md"), "r", encoding="utf8").read(),
-    long_description_content_type="text/markdown",
-    keywords=["testing", "cad"],
-    classifiers=[],
-    package_data={
-        "termin": [
-            "ga201/*",
-            "physics/*",
-            "colliders/*",
-            "fem/*",
-            "kinematic/*",
-            "geombase/*",
-            "utils/*",
-            "geomalgo/*",
-            "linalg/*",
-            "robot/*",
-            "loaders/*",
-            "visualization/*",
-            "visualization/**/*",
-            "mesh/*",
-            "editor/*",
-        ]
-    },
-    include_package_data=True,
-    install_requires=[
-        "numpy",
-        "PyOpenGL>=3.1",
-        "glfw>=2.5.0",
-        "Pillow>=9.0",
-        "pyassimp",
-        "scipy",
-        "PyQt6>=6.4",
-    ],
-    extras_require={
-    },
-    zip_safe=False,
-)
+if __name__ == "__main__":
+    setup(
+        name="termin",
+        packages=["termin"],
+        python_requires='>3.10.0',
+        version="0.0.0",
+        license="MIT",
+        description="Projective geometry library",
+        author="mirmik",
+        author_email="mirmikns@yandex.ru",
+        url="https://github.com/mirmik/termin",
+        long_description=open(os.path.join(
+            directory, "README.md"), "r", encoding="utf8").read(),
+        long_description_content_type="text/markdown",
+        keywords=["testing", "cad"],
+        classifiers=[],
+        package_data={
+            "termin": [
+                "ga201/*",
+                "physics/*",
+                "colliders/*",
+                "fem/*",
+                "kinematic/*",
+                "geombase/*",
+                "utils/*",
+                "geomalgo/*",
+                "linalg/*",
+                "robot/*",
+                "loaders/*",
+                "visualization/*",
+                "visualization/**/*",
+                "mesh/*",
+                "editor/*",
+            ]
+        },
+        include_package_data=True,
+        install_requires=[
+            "numpy",
+            "PyOpenGL>=3.1",
+            "glfw>=2.5.0",
+            "Pillow>=9.0",
+            "pyassimp",
+            "scipy",
+            "PyQt6>=6.4",
+        ],
+        extras_require={
+        },
+        zip_safe=False,
+    )
