257b438bbe7b4fb8e270d0c7135bf067acc0844a
koven <kovenwalker@proton.me>
2025-12-13 12:46:24 -0500

    'a'

--- termin/visualization/core/resources.py
+++ termin/visualization/core/resources.py
@@ -311,9 +311,8 @@ class ResourceManager:
             mesh: Меш
             source_path: Путь к файлу-источнику
         """
-        # Устанавливаем имя на меш если не задано
-        if mesh.name is None:
-            mesh.name = name
+        # Устанавливаем имя на меш (чтобы setter в инспекторе мог его использовать)
+        mesh.name = name
 
         keeper = self.get_or_create_mesh_keeper(name)
         keeper.set_mesh(mesh, source_path)
