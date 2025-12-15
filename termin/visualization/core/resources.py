fe17cc9001724cc98726743581dd69afb6431117
koven <kovenwalker@proton.me>
2025-12-15 18:39:37 -0500

    'a'

--- termin/visualization/core/resources.py
+++ termin/visualization/core/resources.py
@@ -232,7 +232,7 @@ class ResourceManager:
             uniforms=[
                 MaterialProperty("u_color", "Color", (1.0, 1.0, 1.0, 1.0)),
                 MaterialProperty("u_albedo_texture", "Texture", None),
-                MaterialProperty("u_shininess", "Float", 32.0, 1.0, 128.0),
+                MaterialProperty("u_shininess", "Float", 32.0, 1.0, 512.0),
             ],
         )
 
