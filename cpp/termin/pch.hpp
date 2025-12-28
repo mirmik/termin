bae42af9f328e1dcbecaafc1f1dae329e2bc7e61
kuro <mirmikns@yandex.ru>
2025-12-29 01:56:21 +0300

    feat: Update CMake configuration to require CMake 3.16 and add precompiled header support

---
+++ cpp/termin/pch.hpp
@@ -0,0 +1,26 @@
+// Precompiled header for termin
+#pragma once
+
+#ifdef __cplusplus
+
+// STL
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <set>
+#include <functional>
+#include <memory>
+#include <any>
+#include <optional>
+#include <variant>
+#include <algorithm>
+#include <cmath>
+
+// pybind11
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+#include <pybind11/functional.h>
+#include <pybind11/numpy.h>
+
+#endif // __cplusplus
