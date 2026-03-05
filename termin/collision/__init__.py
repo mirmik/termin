c7aa5c1247b3dd71b05172e3bb606b2648c8da12
netricks <mirmikns@yandex.ru>
2026-03-06 00:16:58 +0300

    'ci'

--- termin/collision/__init__.py
+++ termin/collision/__init__.py
@@ -27,6 +27,11 @@ Usage:
 # Setup DLL paths before importing native extensions
 from termin import _dll_setup  # noqa: F401
 
+import os as _os
+_sdk_dir = _os.path.join(_os.sep, "opt", "termin", "lib", "python", "termin", "collision")
+if _os.path.isdir(_sdk_dir) and _sdk_dir not in __path__:
+    __path__.append(_sdk_dir)
+
 from ._collision_native import (
     ContactID,
     ContactPoint,
