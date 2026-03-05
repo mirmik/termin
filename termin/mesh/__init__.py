421795d920cbf5bbe491c576710db508e7d71972
netricks <mirmikns@yandex.ru>
2026-03-06 00:23:48 +0300

    'ci'

--- termin/mesh/__init__.py
+++ termin/mesh/__init__.py
@@ -1,5 +1,10 @@
 """Mesh module - Mesh3, TcMesh."""
 
+import os as _os
+_sdk_dir = _os.path.join(_os.sep, "opt", "termin", "lib", "python", "termin", "mesh")
+if _os.path.isdir(_sdk_dir) and _sdk_dir not in __path__:
+    __path__.append(_sdk_dir)
+
 from tgfx import Mesh3, TcMesh
 from termin.mesh.mesh_component import MeshComponent
 
