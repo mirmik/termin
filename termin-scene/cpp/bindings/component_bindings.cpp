da3a1424ca2d32c80e4f1c1e834c7336461a1b71
netricks <mirmikns@yandex.ru>
2026-03-10 20:03:10 +0300

    'input finish'

--- termin-scene/cpp/bindings/component_bindings.cpp
+++ termin-scene/cpp/bindings/component_bindings.cpp
@@ -11,6 +11,7 @@
 #include <termin/entity/component_registry_python.hpp>
 #include <termin/entity/entity.hpp>
 #include "core/tc_component.h"
+#include "core/tc_input_capability.h"
 
 namespace nb = nanobind;
 
@@ -106,6 +107,9 @@ void bind_component_registry(nb::module_& m) {
             nb::arg("name"), nb::arg("cap_id"),
             "Check whether a component type has a capability")
         .def_static("drawable_capability_id", &ComponentRegistry::drawable_capability_id)
+        .def_static("input_capability_id", []() {
+            return tc_input_capability_id();
+        })
         .def_static("get_types_with_capability", [](tc_component_cap_id cap_id) {
             const char* types[64];
             size_t count = tc_component_registry_get_types_with_capability(cap_id, types, 64);
@@ -139,6 +143,7 @@ void bind_component_registry(nb::module_& m) {
             }
             info["descendants"] = desc_list;
             info["is_drawable"] = tc_component_registry_has_capability(type_name, tc_drawable_capability_id());
+            info["is_input_handler"] = tc_component_registry_has_capability(type_name, tc_input_capability_id());
 
             result.append(info);
         }
