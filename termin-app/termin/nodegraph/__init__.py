7770ad868d292338187f06bfbfaf2b8606d542f2
netricks <mirmikns@yandex.ru>
2026-05-10 11:41:32 +0300

    Remove nodegraph compiler module
    Delete the compiler.py file and its associated exports from the nodegraph package. This removes the logic responsible for converting node graphs into render pipelines.

--- termin-app/termin/nodegraph/__init__.py
+++ termin-app/termin/nodegraph/__init__.py
@@ -3,14 +3,10 @@
 from termin.nodegraph.editor import PipelineGraphEditor
 from termin.nodegraph.viewport_frame import ViewportFrame
 from termin.nodegraph.node import GraphNode, NodeParam
-from termin.nodegraph.compiler import compile_graph, compile_graph_to_dict, CompileError
 
 __all__ = [
     "PipelineGraphEditor",
     "ViewportFrame",
     "GraphNode",
     "NodeParam",
-    "compile_graph",
-    "compile_graph_to_dict",
-    "CompileError",
 ]
