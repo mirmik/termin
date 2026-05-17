6c483edae452a344ace6ba0d327b7e4dc804e912
netricks <mirmikns@yandex.ru>
2026-05-17 20:45:40 +0300

    Remove unused mesh_renderer header from recast_navmesh_builder_component.cpp

--- termin-app/cpp/termin/navmesh/recast_navmesh_builder_component.cpp
+++ termin-app/cpp/termin/navmesh/recast_navmesh_builder_component.cpp
@@ -3,7 +3,6 @@
 #include "navmesh_keeper_component.hpp"
 #include "off_mesh_link_component.hpp"
 #include <components/mesh_component.hpp>
-#include <termin/render/mesh_renderer.hpp>
 #include <termin/render/shader_parser.hpp>
 #include <termin/geom/mat44.hpp>
 #include <termin/tc_scene.hpp>
