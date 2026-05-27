125a19e92e11ca7f2811e161db842202276c59a8
mirmik <mirmikns@yandex.ru>
2026-05-27 16:23:31 +0300

    Export foliage data classes and functions using ENTITY_API. This enables the foliage component to be used across different library boundaries.

--- termin-components/termin-components-foliage/termin/foliage/foliage_file.hpp
+++ termin-components/termin-components-foliage/termin/foliage/foliage_file.hpp
@@ -7,14 +7,14 @@
 
 namespace termin {
 
-struct FoliageFileResult {
+struct ENTITY_API FoliageFileResult {
 public:
     bool ok = false;
     std::string message;
 };
 
-FoliageFileResult load_foliage_file(const std::filesystem::path& path, FoliageData& out);
-FoliageFileResult save_foliage_file(const std::filesystem::path& path, const FoliageData& data);
+ENTITY_API FoliageFileResult load_foliage_file(const std::filesystem::path& path, FoliageData& out);
+ENTITY_API FoliageFileResult save_foliage_file(const std::filesystem::path& path, const FoliageData& data);
 
 } // namespace termin
 
