24103b1e9e742cb8352aa876244bf1b40a53419b
mirmik <mirmikns@yandex.ru>
2026-06-26 18:14:07 +0300

    Add ModuleInputFileProcessor to track native module source changes.
    This introduces a new file processor that monitors C/C++ source files and CMake configurations to mark associated modules as dirty. It also updates ProjectModulesRuntime to support tracking reasons for module dirtiness and provides improved path-to-module mapping.

--- termin-app/termin/editor_core/file_processors/__init__.py
+++ termin-app/termin/editor_core/file_processors/__init__.py
@@ -5,7 +5,10 @@ from termin.editor_core.file_processors.mesh_processor import MeshPreLoader
 from termin.editor_core.file_processors.shader_processor import ShaderPreLoader
 from termin.editor_core.file_processors.texture_processor import TexturePreLoader
 from termin.editor_core.file_processors.component_processor import ComponentFileProcessor
-from termin.editor_core.file_processors.module_processor import ModuleFileProcessor
+from termin.editor_core.file_processors.module_processor import (
+    ModuleFileProcessor,
+    ModuleInputFileProcessor,
+)
 from termin.editor_core.file_processors.pipeline_preloader import PipelinePreLoader
 from termin.editor_core.file_processors.scene_pipeline_preloader import ScenePipelinePreLoader
 from termin.editor_core.file_processors.voxel_grid_processor import VoxelGridPreLoader
@@ -23,6 +26,7 @@ __all__ = [
     "TexturePreLoader",
     "ComponentFileProcessor",
     "ModuleFileProcessor",
+    "ModuleInputFileProcessor",
     "PipelinePreLoader",
     "ScenePipelinePreLoader",
     "VoxelGridPreLoader",
