124a41c3f0887fbb665fa0eb65237dc4c819e242
mirmik <mirmikns@yandex.ru>
2026-08-06 12:10:11 +0300

    fix build

--- termin-components/termin-components-physics/include/termin/physics_components/components_bootstrap.hpp
+++ termin-components/termin-components-physics/include/termin/physics_components/components_bootstrap.hpp
@@ -1,7 +1,9 @@
 #pragma once
 
+#include <termin/export.hpp>
+
 namespace termin {
 
-void register_builtin_physics_component_types();
+ENTITY_API void register_builtin_physics_component_types();
 
 } // namespace termin
